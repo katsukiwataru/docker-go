064bc839dc865c73bc2b574620818b06875074f8
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-02-06 20:03:46 +0900

    feat: add api server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "godocker/api"
+    "net/http"
+
+    "github.com/labstack/echo"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    e := echo.New()
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, world")
+    })
+
+    e.GET("/api", func(c echo.Context) error {
+        return c.String(http.StatusOK, api.GetApi())
+    })
+
+    e.Logger.Fatal(e.Start(":1323"))
 }
